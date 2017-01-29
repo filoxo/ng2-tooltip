1e09a260b40e9e8025040b84ca76b468bb62d756
Carlos Filoteo <cfiloteo@ancestry.com>
2017-01-28 20:28:00 -0700

    Update spacing, lines, and single quotes.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {Tooltip} from "./Tooltip";
-import {TooltipContent} from "./TooltipContent";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tooltip } from './Tooltip';
+import { TooltipContent } from './TooltipContent';
 
-export * from "./Tooltip";
-export * from "./TooltipContent";
+export * from './Tooltip';
+export * from './TooltipContent';
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: [
-        Tooltip,
-        TooltipContent,
-    ],
-    exports: [
-        Tooltip,
-        TooltipContent,
-    ],
-    entryComponents: [
-        TooltipContent
-    ]
+    imports: [ CommonModule ],
+    declarations: [ Tooltip, TooltipContent ],
+    exports: [ Tooltip, TooltipContent ],
+    entryComponents: [ TooltipContent ]
 })
-export class TooltipModule {
-
-}
+export class TooltipModule {}
